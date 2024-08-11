b972ab69f6c506a978ae48b2532c8e27f7f36cf0
sanc7us <sanc7us@gmail.com>
2024-08-10 21:07:48 -0400

    Starting Skill Set section

---
+++ src/layouts/Skillset.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { Title } from "@components/index";
+</script>
+
+<section>
+  <Title>Skill Set</Title>
+</section>
+
+<style>
+  section {
+    margin-top: 12rem;
+  }
+</style>
